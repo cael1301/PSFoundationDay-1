acc7474ced7a344018670877271903de6245c933
cael1301 <64167504+cael1301@users.noreply.github.com>
2020-07-17 15:05:41 -0500

    Add files via upload

---
+++ Day 1 Demo _ _Command Termination and Line Continuation_.ps1
@@ -0,0 +1,13 @@
+
+#Demonstrate command termination using oneline
+Get-service ; get-process; get-childitem
+
+
+#Demonstrate line continuation using “ with no closing “ or {}
+“
+This is line
+Con
+Tin
+""
+“
+""
